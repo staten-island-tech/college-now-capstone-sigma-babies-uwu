cb6c8dbbb45240a74db0b4a8cdddf0d09b953221
Sophie Dong <108767907+GoldenTalos@users.noreply.github.com>
2024-03-06 23:13:32 -0500

    Update mongoose.js

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
 
 mongoose
-  .connect(`${process.env.DATABASE}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  //   .connect(`${process.env.DATABASE}`, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   })
+  .connect(
+    "mongodb+srv://goldentalos:Dong20070922@cluster0.pb9a6bd.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("connected to DB"));
 
 mongoose.connection.on("error", (err) => {

e2a70060a9c841213da82abb2892007652844b6f
Sophie <108767907+GoldenTalos@users.noreply.github.com>
2024-03-07 16:07:14 -0500

    Update users.js

--- Models/users.js
+++ Models/users.js
@@ -41,4 +41,13 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+UserSchema.methods.comparePassword = function (passw, cb) {
+  bcrypt.compare(passw, this.password, function (err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
 module.exports = mongoose.model("User", userSchema);

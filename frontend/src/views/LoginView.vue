1bfc468bf1610c3042e5a996bb2cf8bfbd74ecbf
uh-karsi <90662606+uh-karsi@users.noreply.github.com>
2024-05-02 15:26:15 -0400

    argf

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,8 +1,30 @@
 <template>
   <form action="submit">
     <p>START YOUR SIGMA GRINDSET</p>
-    <input type="text" placeholder="username">
-    <input type="text" placeholder="password">
-    <button>submit</button>
+    <input type="username" placeholder="username" v-model="username" />
+    <input type="password" placeholder="password" v-model="password" />
+    <button @click="login">submit</button>
   </form>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+async function login(username, password) {
+  console.log(username, password)
+  const res = await fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: username.toLowerCase(),
+      password: password
+    })
+  })
+  console.log(res)
+}
+</script>

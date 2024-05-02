0bb9d8c201693b3ff46c2cc0688ce743ce9d21c2
richie <113203265+R1chi333@users.noreply.github.com>
2024-05-02 11:02:16 -0400

    .

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <div class="con">
+    <img class="logo" src="/BuffSeagull.png" alt="Buff Seagull Logo">
+    <h1 class="title">SEAGULL MAXXING</h1>
+  </div>
+</template>
+<style scoped>
+@font-face {
+  font-family: comicSans;
+  src: url(comicsans.ttf);
+}
+.con{
+  margin-left: auto;
+  margin-right: auto;
+  width: 75vw;
+  display: flex;
+  justify-content: center;
+  margin-top: 10vw;
+}
+.title{
+  width: fit-content;
+  display: inline;
+  text-align: center;
+  font-family: comicSans;
+  height: 20vw;
+}
+.logo{
+  width: 20vw;
+  height: 20vw;
+}
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<template>
+  <form action="submit">
+    <p>START YOUR SIGMA GRINDSET</p>
+    <input type="text" placeholder="username">
+    <input type="text" placeholder="password">
+    <button>submit</button>
+  </form>
+</template>

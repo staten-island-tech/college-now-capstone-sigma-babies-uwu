1fbc6332657bd63ebbf099d3d45e778578446a0c
richie <113203265+R1chi333@users.noreply.github.com>
2024-05-16 16:20:21 -0400

    .

--- frontend/src/components/Compose.vue
+++ frontend/src/components/Compose.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="compose">
+        <button class="close">×</button>
         <form action="" class="form">
             <div>
             <p>Title:</p>
-            <input class="title"  type="text" >
+            <textarea class="title" type="text" maxlength="40"> </textarea>
             </div>
             <div>
                 <p>Note:</p>
@@ -17,11 +18,28 @@
   font-family: comicSans;
   src: url(comicsans.ttf);
 }
+.close{
+    position: absolute;
+    top: 1.5vw;
+    right: 2vw;
+    border: none;
+    background: transparent;
+    font-size: 2vw;
+    color: rgb(219, 138, 138);
+    -o-transition-duration: 1s;
+    transition-duration: .2s;
+}
+.close:hover{
+    scale: 1.2;
+}
+.close:active{
+    transform: translateY(.3vw);
+}
 p{
     font-size: 1vw;
     color: rgb(219, 138, 138);
-    text-decoration: underline;
     font-family: comicSans;
+    font-weight: bold;
     
 }
 .compose{
@@ -38,27 +56,30 @@ p{
     margin-top: 3vw;
     margin-left: 1vw;
 }
-input{
-    font-family: comicSans;
-    background: white;
-    border: none;
-    padding: .1vw;
-    vertical-align: top;
-}
 .note{
     border-radius: 1vw;
     box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(219, 138, 138) 0px 0px 0px 1px inset;
-    width: 90%;
-    height: 17vw;
+    width: 85%;
+    height: 16vw;
     font-family: comicSans;
     color: rgb(219, 138, 138);
     font-size: 1vw;
+    padding: 1vw;
 }
-.note:active{
-    border-color:rgb(219, 138, 138);;
+textarea:focus, input:focus{
+    outline: none;
+}
+textarea{
+    resize: none;
 }
 .title{
-    border-radius: .2vw;
+    border-radius: .5vw;
+    width: 85%;
+    height: 1.5vw;
+    font-family: comicSans;
+    color: rgb(219, 138, 138);
+    font-size: 1vw;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(219, 138, 138) 0px 0px 0px 1px inset;
 }
 
 </style>

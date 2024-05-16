3e4d4959f1696f91e3932c2ce2681d8a2ae89384
richie <113203265+R1chi333@users.noreply.github.com>
2024-05-16 16:03:19 -0400

    .

--- frontend/src/components/Compose.vue
+++ frontend/src/components/Compose.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="compose">
-        <form action="">
-            <input type="text" placeholder="TITLE">
-            <input type="text" placeholder="NOTE">
+        <form action="" class="form">
+            <div>
+            <p>Title:</p>
+            <input class="title"  type="text" >
+            </div>
+            <div>
+                <p>Note:</p>
+                <textarea class="note" type="text"> </textarea>
+            </div>
         </form>
     </div>
 </template>
-<style setup>
+<style scoped>
+@font-face {
+  font-family: comicSans;
+  src: url(comicsans.ttf);
+}
+p{
+    font-size: 1vw;
+    color: rgb(219, 138, 138);
+    text-decoration: underline;
+    font-family: comicSans;
+    
+}
 .compose{
     height: 30vw;
     width: 25vw;
@@ -17,7 +34,31 @@
     bottom: 4vw;
     right: 2vw;
 }
-input{
-    display: block;
+.form{
+    margin-top: 3vw;
+    margin-left: 1vw;
 }
+input{
+    font-family: comicSans;
+    background: white;
+    border: none;
+    padding: .1vw;
+    vertical-align: top;
+}
+.note{
+    border-radius: 1vw;
+    box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(219, 138, 138) 0px 0px 0px 1px inset;
+    width: 90%;
+    height: 17vw;
+    font-family: comicSans;
+    color: rgb(219, 138, 138);
+    font-size: 1vw;
+}
+.note:active{
+    border-color:rgb(219, 138, 138);;
+}
+.title{
+    border-radius: .2vw;
+}
+
 </style>
